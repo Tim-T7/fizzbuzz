d3a1b33e2147c21688a548bf371bf19a39de00e3
Timmy Tran <timmy.tran@mrjaffesclass.com>
2023-10-24 11:43:09 -0700

    Initial commit

---
+++ fizzbuzz.java
@@ -0,0 +1,15 @@
+
+/**
+ * Write a description of class fizzbuzz here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class fizzbuzz
+{
+    public static void main (String[] args) {
+        for (int i= 1; i >= 100; i++ ) {
+            
+        }
+    }
+}
