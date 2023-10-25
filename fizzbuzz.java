08d05b5bed18d2b7533369c231d2911b7aeca2e6
Timmy Tran <timmy.tran@mrjaffesclass.com>
2023-10-25 11:28:15 -0700

    Commit message

--- fizzbuzz.java
+++ fizzbuzz.java
@@ -8,8 +8,19 @@
 public class fizzbuzz
 {
     public static void main (String[] args) {
-        for (int i= 1; i >= 100; i++ ) {
-            
+        for (int i= 1; i <= 100; i++ ) {
+            if ((i/15) *15 ==i ) {
+                System.out.println ("FizzyBuzzy");
+            }
+            else if ((i/5) *5 ==i){
+                System.out.println ("Buzzy");
+            }
+            else if ((i/3) *3 ==i) {
+                System.out.println ("Fizzy");
+            }
+            else {
+                System.out.println(i);
+            }
         }
     }
 }
